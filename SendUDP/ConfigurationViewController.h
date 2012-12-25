a197f024d1d622c4a7471b6ba6234e12bd11f7fd
Thomas Mohaupt <thomas.mohaupt@gmail.com>
2012-12-25 15:44:03 +0100

    add ConfigurationViewController

---
+++ SendUDP/ConfigurationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfigurationViewController.h
+//  SendUDP
+//
+//  Created by Thomas Mohaupt on 25.12.12.
+//  Copyright (c) 2012 Thomas Mohaupt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigurationViewController : UITableViewController
+
+@end
---
+++ SendUDP/ConfigurationViewController.m
@@ -0,0 +1,29 @@
+//
+//  ConfigurationViewController.m
+//  SendUDP
+//
+//  Created by Thomas Mohaupt on 25.12.12.
+//  Copyright (c) 2012 Thomas Mohaupt. All rights reserved.
+//
+
+#import "ConfigurationViewController.h"
+
+@interface ConfigurationViewController ()
+
+@end
+
+@implementation ConfigurationViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
