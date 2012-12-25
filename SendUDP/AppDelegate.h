f352f5a9dfaa68a9954938f26cd4684c6a1d5d98
Thomas Mohaupt <thomas.mohaupt@gmail.com>
2012-12-25 14:43:58 +0100

    Initial Commit

---
+++ SendUDP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SendUDP
+//
+//  Created by Thomas Mohaupt on 25.12.12.
+//  Copyright (c) 2012 Thomas Mohaupt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SendUDP/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SendUDP
+//
+//  Created by Thomas Mohaupt on 25.12.12.
+//  Copyright (c) 2012 Thomas Mohaupt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
