b69a0568fd10edc2222c035e512a3ba01e609c0f
Thomas Mohaupt <thomas.mohaupt@gmail.com>
2012-12-31 12:46:27 +0100

    layout

--- SendUDP/SendUdpViewController.m
+++ SendUDP/SendUdpViewController.m
@@ -14,26 +14,23 @@
 
 @implementation SendUdpViewController
 
-NSString *segueToConfigurationView = @"configure";
+NSString* const segueToConfigurationView = @"configure";
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 
 	// Do any additional setup after loading the view.
     [self initConfigButton];
 }
 
-- (void)initConfigButton
-{
+- (void)initConfigButton {
 	self.configButton.title = @"\u2699";
 	UIFont *font = [UIFont fontWithName:@"Helvetica" size:24.0];
 	NSDictionary *dict = [[NSDictionary alloc] initWithObjectsAndKeys:font, UITextAttributeFont, nil];
 	[self.configButton setTitleTextAttributes:dict forState:UIControlStateNormal];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
